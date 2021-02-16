f6e7e693c19ae1bb19b62119f52f09fe5b7fab13
Dan Peet <daniel.peet@criton.com>
2021-02-16 20:30:38 +0000

    Define the interface

---
+++ lib/munros.dart
@@ -0,0 +1,15 @@
+import 'src/data/hills_db.dart';
+import 'src/filters.dart';
+import 'src/models.dart';
+import 'src/queries.dart';
+
+export 'src/filters.dart';
+export 'src/models.dart';
+
+Future<List<Munro>> getMunros({
+  List<MunroSelector> filterBy = const [],
+}) {
+  assert(filterBy != null);
+  final repository = HillsDbLocalRepository();
+  return queryMunros(repository, filterBy);
+}
